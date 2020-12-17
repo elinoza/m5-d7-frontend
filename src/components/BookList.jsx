32412bc6edcc3867b52b44d2d9b63384d15c037d
elinoza <hillcakmak@gmail.com>
2020-12-17 17:16:57 +0300

    Update BookList.jsx

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -12,7 +12,7 @@ class BookList extends React.Component {
 componentDidMount= async()=>{
 
   try{ 
-    let response= await fetch(process.env.REACT_URL)
+    let response= await fetch(process.env.REACT_APP_URL)
     let booklist= await response.json()
     if (response.ok) {
       this.setState({ booklist:booklist})
